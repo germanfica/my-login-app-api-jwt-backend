8b547757adb52245781236921f8154019d89e3fc
German Fica <germafica@gmail.com>
2024-05-25 12:48:08 -0300

    Add error.middleware.ts

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,17 +2,9 @@ import { Router } from 'express';
 import { authenticateLocal, authenticateJwt } from '../auth';
 import { validateUserLogin, validateUserSignUp } from '../utils/validators';
 import * as AuthController from '../controllers/auth.controller';
-import { validationResult } from 'express-validator';
+import { handleValidationErrors } from '../middlewares/error.middleware';
 
 const router = Router();
-
-const handleValidationErrors = (req: any, res: any, next: any) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-};
 
 router.post('/login', validateUserLogin, handleValidationErrors, authenticateLocal, AuthController.login);
 router.post('/signup', validateUserSignUp, handleValidationErrors, AuthController.signup);
